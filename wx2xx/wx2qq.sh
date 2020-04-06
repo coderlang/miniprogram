22afc4038f22a82ae6b924ad9b7534f2a851cd8c
码农浪哥 <coderlang@qq.com>
2020-04-06 17:45:07 +0800

    更新wx2qq

--- wx2xx/wx2qq.sh
+++ wx2xx/wx2qq.sh
@@ -1,17 +1,26 @@
 #!/bin/bash
 
-if [[ $#!=2 ]]; then
-	echo "Usage: $0 wxdir qqdir"
+if [ $# != 1 ]; then
+	echo "Usage: $0 dir"
 	exit 1
 fi
 
-find ../miniprogram -name "*.wxml" | xargs sed -i "" "s/.wxml/.qml/g"
-find ../miniprogram -name "*.wxss" | xargs sed -i "" "s/.wxss/.qss/g"
+sourcedir=$1
+targetdir=$1-qq
 
-find ../miniprogram -name "*.wxml" | xargs sed -i "" "s/wx:/qq:/g"
-find ../miniprogram -name "*.ts" | xargs sed -i "" "s/wx:/qq:/g"
-find ../miniprogram -name "*.js" | xargs sed -i "" "s/wx:/qq:/g"
+rm -rf ${targetdir}
+cp -rf ${sourcedir} ${targetdir}
 
-find ../miniprogram -name "*.wxml" | awk '{a=$0; sub(/wxml$/, "qml", $0); print "mv", a, $0;}' | bash
-find ../miniprogram -name "*.wxss" | awk '{a=$0; sub(/wxss$/, "qss", $0); print "mv", a, $0;}' | bash
+find ${targetdir} -name "*.wxml" | awk '{a=$0; sub(/wxml$/, "qml", $0); print "mv", a, $0;}' | bash
+find ${targetdir} -name "*.wxss" | awk '{a=$0; sub(/wxss$/, "qss", $0); print "mv", a, $0;}' | bash
+find ${targetdir} -name "*.wxs" | awk '{a=$0; sub(/wxs/, "qs", $0); print "mv", a, $0;}' | bash
 
+find ${targetdir} -name "*.wxml" | xargs sed -i "" "s/.wxml/.qml/g"
+find ${targetdir} -name "*.wxss" | xargs sed -i "" "s/.wxss/.qss/g"
+
+find ${targetdir} -name "*.wxml" | xargs sed -i "" "s/wx:/qq:/g"
+find ${targetdir} -name "*.ts" | xargs sed -i "" "s/wx:/qq:/g"
+find ${targetdir} -name "*.js" | xargs sed -i "" "s/wx:/qq:/g"
+
+find ${targetdir} -name "*.wxml" | xargs sed -i "" "s/<wxs/<qs/g"
+find ${targetdir} -name "*.wxml" | xargs sed -i "" "s/</wxs>/</qs>/g"
