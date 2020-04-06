50d65909ff1a428a9fb8496088bc21ebec0e9599
码农浪哥 <coderlang@qq.com>
2020-04-06 17:21:09 +0800

    更新wx2qq

--- wx2xx/wx2qq.sh
+++ wx2xx/wx2qq.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ $#!=2 ]]; then
+	echo "Usage: $0 wxdir qqdir"
+	exit 1
+fi
+
 find ../miniprogram -name "*.wxml" | xargs sed -i "" "s/.wxml/.qml/g"
 find ../miniprogram -name "*.wxss" | xargs sed -i "" "s/.wxss/.qss/g"
 
