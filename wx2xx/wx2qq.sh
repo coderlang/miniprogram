ca6a57a4e93c314ab068223da77841ae945011be
码农浪哥 <coderlang@qq.com>
2020-04-06 17:15:11 +0800

    增加wx2xx

---
+++ wx2xx/wx2qq.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+find ../miniprogram -name "*.wxml" | xargs sed -i "" "s/.wxml/.qml/g"
+find ../miniprogram -name "*.wxss" | xargs sed -i "" "s/.wxss/.qss/g"
+
+find ../miniprogram -name "*.wxml" | xargs sed -i "" "s/wx:/qq:/g"
+find ../miniprogram -name "*.ts" | xargs sed -i "" "s/wx:/qq:/g"
+find ../miniprogram -name "*.js" | xargs sed -i "" "s/wx:/qq:/g"
+
+find ../miniprogram -name "*.wxml" | awk '{a=$0; sub(/wxml$/, "qml", $0); print "mv", a, $0;}' | bash
+find ../miniprogram -name "*.wxss" | awk '{a=$0; sub(/wxss$/, "qss", $0); print "mv", a, $0;}' | bash
+
