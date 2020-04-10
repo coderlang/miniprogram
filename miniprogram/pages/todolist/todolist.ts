49e2f3bce11eb27069a7c5b3a4d8f808d7d37072
码农浪哥 <coderlang@qq.com>
2020-04-10 17:44:02 +0800

    更新todolist

--- miniprogram/pages/todolist/todolist.ts
+++ miniprogram/pages/todolist/todolist.ts
@@ -1,5 +1,13 @@
 Page({
   data:{
     items:["你说", "222", "444"],
+    text:null as string,
+  },
+  onNewTodoInput(e:{detail:{value:string}}):void {
+    console.log("----e", e);
+    this.setData({text:e.detail.value});
+  },
+  onNewTodoConfirm():void {
+    this.setData({items:[...this.data.items, this.data.text], text:null});
   }
 })
