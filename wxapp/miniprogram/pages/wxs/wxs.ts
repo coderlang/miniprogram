068a33bf75e095ee174eda289a6030250a81098f
码农浪哥 <coderlang@qq.com>
2020-07-27 21:13:15 +0800

    数组的使用

--- wxapp/miniprogram/pages/wxs/wxs.ts
+++ wxapp/miniprogram/pages/wxs/wxs.ts
@@ -1,7 +1,9 @@
 Page({
   data: {
     timestamp:new Date().getTime() as number,
-    maoTaiPrice:1700*100 as number
+    maoTaiPrice:1700*100 as number,
+    str1:["一", "二", "三"] as string[],
+    str2:["1", "2", "3"] as string[],
   },
   sell():void {
     this.setData({
