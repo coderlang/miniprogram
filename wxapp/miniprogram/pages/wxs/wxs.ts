e5cba1d204ac21dbb3d3bd7dffc0863b9e16f77e
码农浪哥 <coderlang@qq.com>
2020-07-29 21:31:49 +0800

    删除数组操作

--- wxapp/miniprogram/pages/wxs/wxs.ts
+++ wxapp/miniprogram/pages/wxs/wxs.ts
@@ -2,8 +2,6 @@ Page({
   data: {
     timestamp:new Date().getTime() as number,
     maoTaiPrice:1700*100 as number,
-    str1:["一", "二", "三"] as string[],
-    str2:["1", "2", "3"] as string[],
   },
   sell():void {
     this.setData({
@@ -13,16 +11,6 @@ Page({
   buy():void {
     this.setData({
       maoTaiPrice:1700*100
-    })
-  },
-  del1():void {
-    this.setData({
-      str1:this.data.str1.filter((r) => r!=='一')
-    })
-  },
-  add1():void {
-    this.setData({
-      str1:['一', ...this.data.str1]
     })
   }
 })
